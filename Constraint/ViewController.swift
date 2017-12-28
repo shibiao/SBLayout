37539e081bd0f9fd9eaf6a58a16e182ac0cc4b68
WalkWithWind <956035125@qq.com>
2017-12-28 09:15:42 +0800

    init commit

---
+++ Constraint/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Constraint
+//
+//  Created by sycf_ios on 2017/12/27.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let redView = UIView()
+        redView.backgroundColor = .red  
+        self.view.addSubview(redView)
+        redView.height(100).width(200).centerX(equalTo: view.centerX).top(equalTo: view.top, constant: 100).end()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
---
+++ SBLayout.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+    s.name         = 'SBLayout'
+    s.version      = '1.0'
+    s.summary      = 'AutoLayout by swift version'
+    s.homepage     = 'https://github.com/shibiao/SBLayout'
+    s.license      = ''
+    s.authors      = {'Shi Biao' => '956035125@qq.com'}
+    s.source       = {:git => 'https://github.com/shibiao/SBLayout.git', :tag => s.version}
+    s.source_files = 'SBLayout/*.{swift}'
+
+    s.ios.deployment_target = '9.0'
+    s.osx.deployment_target = '10.11'
+
+    s.requires_arc = true
+
+end
