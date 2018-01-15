9b083c95497cf7e0059f849b0b6ed9bf1b91ebe3
WalkWithWind <956035125@qq.com>
2018-01-15 15:45:37 +0800

    update demo

--- SBLayoutDemo/SBLayoutDemo/ViewControllers/AutoCellHieghtTableViewController.swift
+++ SBLayoutDemo/SBLayoutDemo/ViewControllers/AutoCellHieghtTableViewController.swift
@@ -12,20 +12,26 @@ class AutoCellHieghtTableViewController: UITableViewController {
     
     var data: [AutoHeightCellModel] = {
         var data = Array<AutoHeightCellModel>()
-        let model1 = AutoHeightCellModel(imageName: "avatar", contentString: "AutoLayout for iOS by swift，support iOS9 and above")
-        let model2 = AutoHeightCellModel(imageName: "avatar", contentString: "SBLayout是用Swift4所写的iOS版本轻量级AutoLayout单文件框架，总代码700多行，非常简单易用！            支持iOS9及其以上版本")
-        let model3 = AutoHeightCellModel(imageName: "avatar", contentString: "使用SBLayout你可以一行代码搞定无论多复杂的代码约束")
+        let model1 = AutoHeightCellModel(imageName: "avatar", contentString:
+            "AutoLayout for iOS by swift，support iOS9 and above"
+        )
+        let model2 = AutoHeightCellModel(imageName: "avatar", contentString:
+            "SBLayout是用Swift4所写的iOS版本轻量级AutoLayout单文件框架，总代码700多行，非常简单易用！            支持iOS9及其以上版本"
+        )
+        let model3 = AutoHeightCellModel(imageName: "avatar", contentString:
+            "使用SBLayout你可以一行代码搞定无论多复杂的代码约束"
+        )
         let model4 = AutoHeightCellModel(imageName: "avatar", contentString: """
-         CocoaPods：
-         导入项目中的方法：
-         第一步：Podfile中添加
-         pod 'SBLayout', '~>4.4'
-         并使用命令pod install完成导入
+             CocoaPods：
+             导入项目中的方法：
+             第一步：Podfile中添加
+             pod 'SBLayout', '~>4.4'
+             并使用命令pod install完成导入
          """
         )
         let model5 = AutoHeightCellModel(imageName: "avatar", contentString:
          """
-         第二步：import SBLayout 或者直接拖拽SBLayout.swift到项目中不需导入头文件即可使用。
+             第二步：import SBLayout 或者直接拖拽SBLayout.swift到项目中不需导入头文件即可使用。
          """
         )
         data.append(model1)
